91e36d70da9a8e0fd733943a09df11e3bf8f9072
mbest80 <116742768+mbest80@users.noreply.github.com>
2022-11-08 19:11:11 +0100

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -20,7 +20,7 @@ export class ProductDetailsComponent implements OnInit {
 
     //Find the product that correspond with the id provided in route.
     this.product = products.find(
-      (product) => product.id === productIdFromRoute
+      product => product.id === productIdFromRoute
     );
   }
 }
